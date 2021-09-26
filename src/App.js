93f20220b109a7baead4f6e4ac113e12c1ac866a
Petrov Kirill <ottokirik@gmail.com>
2021-09-27 01:50:54 +0300

    Created Board model in store

--- src/App.js
+++ src/App.js
@@ -2,9 +2,10 @@ import { useStore } from 'hooks/useStore';
 import { observer } from 'mobx-react-lite';
 
 function App() {
-  const { users } = useStore();
+  const { users, boards } = useStore();
 
-  console.log(users.toJSON());
+  // console.log('users', users.toJSON());
+  console.log('boards', boards.active?.toJSON());
 
   return <div>Работает</div>;
 }
--- src/store/board.js
+++ src/store/board.js
@@ -1,5 +1,55 @@
-import { types } from 'mobx-state-tree';
+import { apiService } from 'api';
+import { types, flow, getParent } from 'mobx-state-tree';
 
-const BoardStore = types.model('BoardStore', {});
+const Task = types.model('Task', {
+  id: types.identifier,
+  title: types.string,
+  description: types.string,
+  assignee: types.string,
+});
+
+const BoardSection = types
+  .model('BoardSection', {
+    id: types.identifier,
+    title: types.string,
+    tasks: types.array(Task),
+  })
+  .actions((self) => {
+    return {
+      load: flow(function* () {
+        const { id: boardID } = getParent(self, 2);
+        const { id: status } = self;
+
+        const { tasks } = yield apiService.get(
+          `boards/${boardID}/tasks/${status}`
+        );
+
+        self.tasks = tasks;
+      }),
+    };
+  });
+
+const Board = types.model('Board', {
+  id: types.identifier,
+  title: types.string,
+  sections: types.array(BoardSection),
+});
+
+const BoardStore = types
+  .model('BoardStore', {
+    boards: types.array(Board),
+    active: types.safeReference(Board),
+  })
+  .actions((self) => {
+    return {
+      load: flow(function* () {
+        self.boards = yield apiService.get('boards');
+        self.active = 'MAIN';
+      }),
+      afterCreate() {
+        self.load();
+      },
+    };
+  });
 
 export { BoardStore };
